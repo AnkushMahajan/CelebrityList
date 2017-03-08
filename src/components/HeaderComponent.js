c0db6f2abc2d19df039cd49a28dffca5e8c7de32
Ankush Mahajan <vish_sukhoi@yahoo.co.in>
2017-03-09 09:41:18 +1100

    Removed a title

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,7 +3,6 @@ import React from 'react';
 let Header = (props) => {
     return (
         <div className="test-header">
-            <h3>{props.title1}</h3>
             <h3>{props.title2}</h3>
             <h4>{props.description}</h4>
             <h6>Reference: <a href={props.referenceLink}> {props.referenceLink}</a></h6>
